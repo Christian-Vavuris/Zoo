fe5d6d700d5f5ad5935a27b73695dc9e5f30012f
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-07-27 20:48:46 -0700

    added in a second route for querying by ID

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray; 
@@ -33,15 +34,30 @@ if (query.name) {
   return filteredResults;    
 };
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
+
+// these are routes 
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
-    
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
-
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result){
+        res.json(result);
+    } 
+    else {
+        res.sendStatus(404);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
